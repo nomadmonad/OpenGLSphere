99b0dae4398715fd11508744390667828fe2717a
Tsuyoshi Yamamoto <tsuyoshi.yamamoto@jrits.ricoh.co.jp>
2014-05-15 12:57:28 +0900

    initial commit

---
+++ OpenGLSphere/OpenGLSphere/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGLSphere
+//
+//  Created by tyamamo on 2014/05/15.
+//  Copyright (c) 2014年 RICOH IT Solutions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpenGLSphere/OpenGLSphere/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  OpenGLSphere
+//
+//  Created by tyamamo on 2014/05/15.
+//  Copyright (c) 2014年 RICOH IT Solutions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
